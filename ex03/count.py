73b5a1653d19c24add80a4f32fa1c049e8511720
Vanessa Rodrigues <varodrig@made-f0Br8s13.clusters.42paris.fr>
2025-02-11 17:09:33 +0100

    -

--- ex03/count.py
+++ ex03/count.py
@@ -1,35 +1,49 @@
+import string
 import sys
 
-def text_analyzer (str):
+# == compares the values
+# is compares the memory 
+# In Python, None is a unique object (there is only one None in memory)
+
+# If the caller doesn't pass a value for text, the function will use None.
+# It allows the function to handle cases where no argument is provided without raising an error. 
+def text_analyzer (text=None):
     '''
     This function counts the number of upper characters, lower characters,
     punctuation and spaces in a given text.
     '''
-    if len(sys.argv) == 1 or sys.argv[1] == "":
-        user_input = input("What is the text to analyze?")
-    else:
-        user_input = sys.argv[1:]
+    if text is None:
+        text = input("What is the text to analyze?\n")
+    if not isinstance(text, str):
+        print("Error: The argument must be a string.")
+        return
     printable = 0
     upper_char = 0
     lower_char = 0
     punctuation = 0
     spaces = 0
 
-    for char in user_input:
-        if char.isprintable() == False:
-            break
-        else
+    for char in text:
+        if char.isprintable():
             printable+= 1
         if char.isupper():
             upper_char+= 1
         elif char.islower():
             lower_char+= 1
-        elif char.punctuation():
+        elif char in string.punctuation:
             punctuation+= 1
         elif char.isspace():
             spaces+= 1
-    print(The text contains {printable} printable character(s):)
-    print(- {upper_char} upper letter(s))
-    print(- {lower_char} lower letter(s))
-    print(- {punctuation} punctuation mark(s))
-    print(- {spaces} space(s))
+    print(f"The text contains {printable} printable character(s):")
+    print(f"- {upper_char} upper letter(s)")
+    print(f"- {lower_char} lower letter(s)")
+    print(f"- {punctuation} punctuation mark(s)")
+    print(f"- {spaces} space(s)")
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        print("Error: Too many arguments.")
+    elif len(sys.argv) == 2:
+        text_analyzer(sys.argv[1])
+    else:
+        text_analyzer()
